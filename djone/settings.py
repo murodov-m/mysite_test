16dd2129e99e8db5d145732d20468b999eba0f4e
Mamur <mamur.m@gmail.com>
2022-05-25 16:05:25 +0500

    adding some files

--- djone/settings.py
+++ djone/settings.py
@@ -40,10 +40,6 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 ]
 
-
-
-
-
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
