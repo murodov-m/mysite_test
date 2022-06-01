c7b31c5ffd76266db45f28431bebb2e340aa4b4b
Mamur <mamur.m@gmail.com>
2022-06-01 21:06:03 +0500

    adding results.html, changing some stuff in views.py

--- polls/views.py
+++ polls/views.py
@@ -2,31 +2,25 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.urls import reverse
 from .models import Question, Choice
 from django.shortcuts import render, get_object_or_404
+from django.views import generic
 
 
-def index(request):
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    print(latest_question_list)
-    context = {
-        'latest_question_list': latest_question_list,
-    }
-    return render(request, 'polls/index.html', context)
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        return Question.objects.order_by('-pub_date')[:5]
 
 
-def detail(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    # try:
-    #     q = Question.objects.get(pk=question_id)
-    # except:
-    #     raise Http404(
-    #         "Не смог найти данный вопрос."
-    #     )
-    return render(request, 'polls/detail.html', {'question': question})
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
 
 
-def results(request, question_id):
-    response = 'Results page'
-    return HttpResponse('You\'re looking at the results page')
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
 
 
 def vote(request, question_id):
