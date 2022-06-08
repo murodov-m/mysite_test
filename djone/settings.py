382f20057817c38ec3dc2c4aee05bd38e3a9124e
Mamur <mamur.m@gmail.com>
2022-06-08 20:23:38 +0500

    gitignore

--- djone/settings.py
+++ djone/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +28,7 @@ SECRET_KEY = 'django-insecure-a_isah6j7nhjm+=(l$r091opv^i!*lgx$e$5@6lynj1qrikgbd
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://super-awesome-site.herokuapp.com/', '127.0.0.1:8000', 'localhost']
 
 
 # Application definition
